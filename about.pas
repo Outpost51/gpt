958e95cf31fcd00e4dc45e7c252ca1b930ea970e
Outpost51 <130508081+Outpost51@users.noreply.github.com>
2023-04-11 16:52:05 -0400

    Add files via upload

---
+++ about.pas
@@ -0,0 +1,33 @@
+unit About;
+
+interface
+
+uses Windows, SysUtils, Classes, Graphics, Forms, Controls, StdCtrls,
+  Buttons, ExtCtrls;
+
+type
+  TAboutBox = class(TForm)
+    Panel1: TPanel;
+    ProductName: TLabel;
+    Version: TLabel;
+    Copyright: TLabel;
+    OKButton: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  AboutBox: TAboutBox;
+
+implementation
+
+   
+{$R *.DFM}
+
+end.
+ 
